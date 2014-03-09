0684162ef5cfec24fa083eaf79b061dd8af82200
Alex Sippel <asippel@narrativescience.com>
2014-03-08 18:28:45 -0600

    first commit

---
+++ static/js/Rplot.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+
+	$('#Rplot').scianimator({
+	    'images': ['static/images/Rplot1.png', 'static/images/Rplot2.png', 'static/images/Rplot3.png', 'static/images/Rplot4.png', 'static/images/Rplot5.png', 'static/images/Rplot6.png', 'static/images/Rplot7.png', 'static/images/Rplot8.png', 'static/images/Rplot9.png', 'static/images/Rplot10.png', 'static/images/Rplot11.png', 'static/images/Rplot12.png', 'static/images/Rplot13.png', 'static/images/Rplot14.png', 'static/images/Rplot15.png', 'static/images/Rplot16.png', 'static/images/Rplot17.png', 'static/images/Rplot18.png', 'static/images/Rplot19.png', 'static/images/Rplot20.png', 'static/images/Rplot21.png', 'static/images/Rplot22.png', 'static/images/Rplot23.png', 'static/images/Rplot24.png'],
+	    'width': 480,
+	    'delay': 1000,
+	    'loopMode': 'loop',
+ 'controls': ['first', 'previous', 'play', 'next', 'last', 'loop', 'speed'], 'delayMin': 0
+	});
+    });
+})(jQuery);
