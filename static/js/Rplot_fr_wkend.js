2fc712a02c3f0ffbd478cf25bf6f0f13f8b232ba
Alex Sippel <asippel@narrativescience.com>
2014-03-10 00:59:17 -0500

    added more animations, added navbar

---
+++ static/js/Rplot_fr_wkend.js
@@ -0,0 +1,40 @@
+
+(function($) {
+    $(document).ready(function() {
+
+	$('#Rplot_fr_wkend').scianimator({
+	    'images': [
+		'static/images/fr_wkend/fr_wkend1.png',
+		'static/images/fr_wkend/fr_wkend2.png',
+		'static/images/fr_wkend/fr_wkend3.png',
+		'static/images/fr_wkend/fr_wkend4.png',
+		'static/images/fr_wkend/fr_wkend5.png',
+		'static/images/fr_wkend/fr_wkend6.png',
+		'static/images/fr_wkend/fr_wkend7.png',
+		'static/images/fr_wkend/fr_wkend8.png',
+		'static/images/fr_wkend/fr_wkend9.png',
+		'static/images/fr_wkend/fr_wkend10.png',
+		'static/images/fr_wkend/fr_wkend11.png',
+		'static/images/fr_wkend/fr_wkend12.png',
+		'static/images/fr_wkend/fr_wkend13.png',
+		'static/images/fr_wkend/fr_wkend14.png',
+		'static/images/fr_wkend/fr_wkend15.png',
+		'static/images/fr_wkend/fr_wkend16.png',
+		'static/images/fr_wkend/fr_wkend17.png',
+		'static/images/fr_wkend/fr_wkend18.png',
+		'static/images/fr_wkend/fr_wkend19.png',
+		'static/images/fr_wkend/fr_wkend20.png',
+		'static/images/fr_wkend/fr_wkend21.png',
+		'static/images/fr_wkend/fr_wkend22.png',
+		'static/images/fr_wkend/fr_wkend23.png',
+		'static/images/fr_wkend/fr_wkend24.png'
+	    ],
+	    'width': 400,
+	    'delay': 500,
+	    'loopMode': 'loop',
+	    'controls': ['play', 'loop', 'speed'],
+	    'delayMin': 0,
+	    'theme': 'blue'
+	});
+    });
+})(jQuery);
