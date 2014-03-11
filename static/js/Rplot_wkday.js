777be46fec25e4dfd4debf4262fcd37c69b1cdde
Alex Sippel <asippel@narrativescience.com>
2014-03-11 02:22:18 -0500

    removed unnecessary Rplot js files, added new ones

---
+++ static/js/Rplot_wkday.js
@@ -0,0 +1,40 @@
+(function($) {
+    $(document).ready(function() {
+
+	$('#Rplot_wkday').scianimator({
+	    'images': [
+		'static/images/wkday/wkday1.png',
+		'static/images/wkday/wkday2.png',
+		'static/images/wkday/wkday3.png',
+		'static/images/wkday/wkday4.png',
+		'static/images/wkday/wkday5.png',
+		'static/images/wkday/wkday6.png',
+		'static/images/wkday/wkday7.png',
+		'static/images/wkday/wkday8.png',
+		'static/images/wkday/wkday9.png',
+		'static/images/wkday/wkday10.png',
+		'static/images/wkday/wkday11.png',
+		'static/images/wkday/wkday12.png',
+		'static/images/wkday/wkday13.png',
+		'static/images/wkday/wkday14.png',
+		'static/images/wkday/wkday15.png',
+		'static/images/wkday/wkday16.png',
+		'static/images/wkday/wkday17.png',
+		'static/images/wkday/wkday18.png',
+		'static/images/wkday/wkday19.png',
+		'static/images/wkday/wkday20.png',
+		'static/images/wkday/wkday21.png',
+		'static/images/wkday/wkday22.png',
+		'static/images/wkday/wkday23.png',
+		'static/images/wkday/wkday24.png'
+	    ],
+	    'width': 790,
+	    'delay': 500,
+	    'loopMode': 'loop',
+	    'controls': ['play', 'loop', 'speed', 'navigator'],
+	    'delayMin': 0,
+	    'theme': 'blue'
+
+	});
+    });
+})(jQuery);
---
+++ static/js/Rplot_wkday_small.js
@@ -0,0 +1,40 @@
+(function($) {
+    $(document).ready(function() {
+
+	$('#Rplot_wkday_small').scianimator({
+	    'images': [
+		'static/images/wkday/wkday1_small.png',
+		'static/images/wkday/wkday2_small.png',
+		'static/images/wkday/wkday3_small.png',
+		'static/images/wkday/wkday4_small.png',
+		'static/images/wkday/wkday5_small.png',
+		'static/images/wkday/wkday6_small.png',
+		'static/images/wkday/wkday7_small.png',
+		'static/images/wkday/wkday8_small.png',
+		'static/images/wkday/wkday9_small.png',
+		'static/images/wkday/wkday10_small.png',
+		'static/images/wkday/wkday11_small.png',
+		'static/images/wkday/wkday12_small.png',
+		'static/images/wkday/wkday13_small.png',
+		'static/images/wkday/wkday14_small.png',
+		'static/images/wkday/wkday15_small.png',
+		'static/images/wkday/wkday16_small.png',
+		'static/images/wkday/wkday17_small.png',
+		'static/images/wkday/wkday18_small.png',
+		'static/images/wkday/wkday19_small.png',
+		'static/images/wkday/wkday20_small.png',
+		'static/images/wkday/wkday21_small.png',
+		'static/images/wkday/wkday22_small.png',
+		'static/images/wkday/wkday23_small.png',
+		'static/images/wkday/wkday24_small.png'
+	    ],
+	    'width': 790,
+	    'delay': 500,
+	    'loopMode': 'loop',
+	    'controls': ['play', 'loop', 'speed', 'navigator'],
+	    'delayMin': 0,
+	    'theme': 'blue'
+
+	});
+    });
+})(jQuery);
