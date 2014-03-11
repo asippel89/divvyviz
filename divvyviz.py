840d926a40d82aeb08a1ebde0bfcc27004728364
Alex Sippel <asippel@narrativescience.com>
2014-03-11 02:19:43 -0500

    double wkday plot, link to large, css fixes

--- divvyviz.py
+++ divvyviz.py
@@ -7,8 +7,13 @@ app = flask.Flask(__name__)
 def divvyviz():
     return flask.render_template("index.html")
 
+@app.route("/wkday")
+def render_large_weekday():
+    return flask.render_template("large_wkday.html")
+
 
 if __name__ == "__main__":
 
+    app.debug = True
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
